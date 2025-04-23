5cc5f87be62c679b69197216eecd587a38d40115
EuricoProg <euricomaciel1@gmail.com>
2025-04-23 21:07:45 +0000

    criando printf

--- CartaSuperTriunfo.c/super_trunfo.c
+++ CartaSuperTriunfo.c/super_trunfo.c
@@ -3,7 +3,7 @@
 typedef struct {
     char estado;
     char codigo[5];
-    char cidade[20];
+    char nomeCidade[20];
     int populacao;
     float area;
     float pib;
@@ -16,8 +16,27 @@ int main(){
 
     printf("Cadastro da carta 1:\n");
 
-    printf("estado (A-H): ");
+    printf("estado: ");
     scanf("%c", &carta1.estado);
 
+    printf("codigo da carta: ");
+    scanf("%s", carta1.codigo);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", carta1.nomeCidade);
+
+    printf("População: ");
+    scanf("%d", &carta1.populacao);
+
+    printf("Área (em km²): ");
+    scanf("%f", &carta1.area);
+
+    printf("PIB (em bilhões):");
+    scanf("%f", &carta1.pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &carta1.PontosTuristicos);
+
     
+
 }
