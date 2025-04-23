c6344ebc2220e3065b74b28f342930cc4438d71d
EuricoProg <euricomaciel1@gmail.com>
2025-04-23 00:34:04 +0000

    Criei Printf

---
+++ CartaSuperTriunfo.c/super_trunfo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   char estado;
+   char código[5];
+   char NomeCidade[50];
+   int populacão;
+   float area;
+   float pib;
+   int PontosTuristicos;
+
+} Carta;
+
+int main(){
+    Carta 
+
+}
