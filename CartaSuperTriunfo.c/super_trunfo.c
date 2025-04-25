d5f7e793c6c4c398320a3ed2d2b358604966f0e9
EuricoProg <euricomaciel1@gmail.com>
2025-04-25 00:45:17 +0000

    conclusao

--- CartaSuperTriunfo.c/super_trunfo.c
+++ CartaSuperTriunfo.c/super_trunfo.c
@@ -61,5 +61,25 @@ int main(){
     printf("Número de Pontos Turisticos: ");
     scanf("%d", &carta2.PontosTuristicos);
 
+    // --- Exibição das cartas ---
     
+    printf("\n Carta 1 \n");
+    printf("Estado: %c\n", carta1.estado);
+    printf("Código: %s\n", carta1.codigo);
+    printf("Nome da cidade: %s\n", carta1.nomeCidade);
+    printf("População: %d\n", carta1.populacao);
+    printf("Área: %.2f km²\n", carta1.area);
+    printf("PIB: %.2f bilhões de reais\n", carta1.pib);
+    printf("Número de pontos turisticos: %d\n", carta1.PontosTuristicos);
+
+    printf("\n Carta 2 \n");
+    printf("Estado: %c\n", carta2.estado);
+    printf("Código: %s\n", carta2.codigo);
+    printf("Nome da cidade: %s\n", carta2.nomeCidade);
+    printf("População: %d", carta2.populacao);
+    printf("Área: %.2f km²\n", carta2.area);
+    printf("PIB: %.2f bilhões de reais\n", carta2.pib);
+    printf("Numero pontos turisticos: %d\n", carta2.PontosTuristicos);
+
+    return 0;
 }
